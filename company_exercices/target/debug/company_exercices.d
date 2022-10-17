c853034681674718a72fc1c68e704988c46d5c7c
Adam <adamalimov@mail.ru>
2022-10-17 17:55:56 +0900

    new file:   company_exercices/Cargo.lock 	new file:   company_exercices/Cargo.toml 	new file:   company_exercices/src/main.rs 	new file:   company_exercices/target/.rustc_info.json 	new file:   company_exercices/target/CACHEDIR.TAG 	new file:   company_exercices/target/debug/.cargo-lock 	new file:   company_exercices/target/debug/.fingerprint/company_exercices-074997d794716b4e/bin-company_exercices 	new file:   company_exercices/target/debug/.fingerprint/company_exercices-074997d794716b4e/bin-company_exercices.json 	new file:   company_exercices/target/debug/.fingerprint/company_exercices-074997d794716b4e/dep-bin-company_exercices 	new file:   company_exercices/target/debug/.fingerprint/company_exercices-074997d794716b4e/invoked.timestamp 	new file:   company_exercices/target/debug/company_exercices 	new file:   company_exercices/target/debug/company_exercices.d 	new file:   company_exercices/target/debug/deps/company_exercices-074997d794716b4e 	new file:   company_exercices/target/debug/deps/company_exercices-074997d794716b4e.d 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/2xtplm12sazhsphq.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/3y44nblp96ugslym.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/3z8d0z4w0qphgzoi.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/4e8w1vu3jae7pspy.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/4g8fkjhdjs4ml0s1.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/5fk53iwejx2g0ld2.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/dep-graph.bin 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/query-cache.bin 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/tzke2qai4q3zpi3.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/work-products.bin 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05-32zl9urbeqvrq/zvee2nofa7jyo64.o 	new file:   company_exercices/target/debug/incremental/company_exercices-3i33jl7tefmdr/s-gejo5ntcpa-l30z05.lock

---
+++ company_exercices/target/debug/company_exercices.d
@@ -0,0 +1,1 @@
+/home/abdulboriy/Desktop/project/company_exercices/target/debug/company_exercices: /home/abdulboriy/Desktop/project/company_exercices/src/main.rs
---
+++ company_exercices/target/debug/deps/company_exercices-074997d794716b4e.d
@@ -0,0 +1,5 @@
+/home/abdulboriy/Desktop/project/company_exercices/target/debug/deps/company_exercices-074997d794716b4e: src/main.rs
+
+/home/abdulboriy/Desktop/project/company_exercices/target/debug/deps/company_exercices-074997d794716b4e.d: src/main.rs
+
+src/main.rs:

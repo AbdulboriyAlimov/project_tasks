d211c590f3f7e77a89371bae623bdb0c10743c5e
Adam <adamalimov@mail.ru>
2022-11-22 14:59:34 +0900

    Fixed previous issues and new project initial code part

---
+++ company_exercices/target/debug/deps/company_exercices-7cf9e827b1fe7600.d
@@ -0,0 +1,5 @@
+/home/abdulboriy/Desktop/project/company_exercices/target/debug/deps/company_exercices-7cf9e827b1fe7600.rmeta: src/main.rs
+
+/home/abdulboriy/Desktop/project/company_exercices/target/debug/deps/company_exercices-7cf9e827b1fe7600.d: src/main.rs
+
+src/main.rs:

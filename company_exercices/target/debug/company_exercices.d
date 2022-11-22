e8ec0a1ae1990daded08781ddb35cd58f8f79438
Adam <adamalimov@mail.ru>
2022-11-22 16:11:27 +0900

    removing old file version

--- company_exercices/target/debug/company_exercices.d
+++
@@ -1,1 +0,0 @@
-/home/abdulboriy/Desktop/project/company_exercices/target/debug/company_exercices: /home/abdulboriy/Desktop/project/company_exercices/src/main.rs
--- company_exercices/target/debug/deps/company_exercices-c0de78451e86e162.d
+++
@@ -1,5 +0,0 @@
-/home/abdulboriy/Desktop/project/company_exercices/target/debug/deps/company_exercices-c0de78451e86e162.rmeta: src/main.rs
-
-/home/abdulboriy/Desktop/project/company_exercices/target/debug/deps/company_exercices-c0de78451e86e162.d: src/main.rs
-
-src/main.rs:

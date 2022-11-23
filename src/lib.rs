f02ed7a13ad30a2d82a12fcf03d23bf5d91c01c0
Adam <adamalimov@mail.ru>
2022-11-23 14:31:43 +0900

    Implemented cargo fmt

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Bccp<T> {
+    left_node: T,
+    right_node: T,
+}
+
+pub struct BccpResult {
+    closest_pairs: ((f64, f64), (f64, f64)),
+    pair_distance: f64,
+}
+
+impl<T> Bccp<T>
+where
+    T: Iterator<Item = (f64, f64)> + Copy,
+{
+    pub fn calculate_distance(&self) -> BccpResult {
+        let mut distance = BccpResult {
+            closest_pairs: ((0., 0.), (0., 0.)),
+            pair_distance: 0.,
+        };
+        let mut temp_var: bool = true;
+
+        for left_point in self.left_node {
+            for right_point in self.right_node {
+                let euclidean_dist = ((left_point.1 - left_point.0).powf(2.)
+                    + (right_point.1 - right_point.0).powf(2.))
+                .sqrt();
+                if temp_var {
+                    distance.pair_distance = euclidean_dist;
+                    distance.closest_pairs = (left_point, right_point);
+                    temp_var = false;
+                } else if euclidean_dist < distance.pair_distance {
+                    distance.pair_distance = euclidean_dist;
+                    distance.closest_pairs = (left_point, right_point);
+                }
+            }
+        }
+        distance
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
